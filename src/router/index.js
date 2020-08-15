ce87fc22d61c688744fe2de4bc3c9772797962b1
sjp <1375221727@qq.com>
2020-08-15 17:16:27 +0800

    three

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import LoginNews from "@/components/login/LoginNews";
+import ListNews from "@/components/list/ListNews";
+import PersinfoNews from "@/components/persinfo/PersinfoNews";
+
 
 Vue.use(Router)
 
@@ -10,6 +14,22 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
-    }
+    },
+    {
+      path: '/news/login',
+      name: 'LoginNews',
+      component: LoginNews
+    },
+    {
+      path: '/news/list',
+      name: 'ListNews',
+      component: ListNews
+    },
+    {
+      path: '/news/persinfo',
+      name: 'PersinfoNews',
+      component: PersinfoNews
+    },
+
   ]
 })
